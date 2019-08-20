d917ddbe42445190beca6183b3489813f47bc7a5
Hubert <olkiewiczhubert@gmail.com>
2019-08-20 12:37:43 +0200

    finally working yarn/npm after accidental package upgrade...

--- frontapp/src/index.tsx
+++ frontapp/src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'react-table/react-table.css'
 
 ReactDOM.render(
     <App/>,
