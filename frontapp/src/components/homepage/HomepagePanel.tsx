b07e2ab461daeb57b48820dfd3c12a058bf6598d
Hubert Olkiewicz <taras14@gmail.com>
2019-08-21 00:47:37 +0200

    moved items and changed layout of homepage panel

--- frontapp/src/components/homepage/HomepagePanel.tsx
+++ frontapp/src/components/homepage/HomepagePanel.tsx
@@ -8,7 +8,6 @@ import {
     CardBody,
     CardHeader,
     CardText,
-    CardTitle,
     Col,
     Container,
     Fade,
@@ -16,7 +15,9 @@ import {
     Input,
     InputGroup,
     Label,
-    Row
+    ListGroup,
+    Row,
+    Tooltip
 } from 'reactstrap';
 import Axios from "axios-observable";
 // @ts-ignore
@@ -24,6 +25,7 @@ import {EventSourcePolyfill} from 'event-source-polyfill';
 import {getRequestHeaderToken} from "security/TokenUtil";
 import ShipForm from "./ShipForm";
 import "./homepagepanel.css"
+import ListStageGroupItem from "./ListStageGroupItem";
 
 interface BattleHistoryDTO {
     battleRecapMap: {
@@ -32,15 +34,20 @@ interface BattleHistoryDTO {
     },
     startDate: Date,
     endDate: Date,
-    isIssued: boolean,
-    stageIssueTime: number
+    isIssued: boolean
+}
+
+export interface BattleHistoryRecapString {
+    stage: string,
+    stageSummary: string
 }
 
 export default class HomepagePanel extends React.Component<RouteComponentProps, any> {
     public readonly state = {
-        battleText: "",
+        battleText: [] as BattleHistoryRecapString[],
         currentBattleVisible: false,
-        stageIssueTime: 5
+        stageIssueTime: 5,
+        tooltipOpen: false
     };
 
     private shipForm1: React.RefObject<any> = React.createRef();
@@ -52,11 +59,10 @@ export default class HomepagePanel extends React.Component<RouteComponentProps, 
         this.getCurrentBattleRequest()
             .subscribe(value => {
                 const dto: BattleHistoryDTO = JSON.parse(value.data);
-                console.info(dto)
-                const text = this.battleHistoryToString(dto);
+                const reacapStringList: BattleHistoryRecapString[] = this.battleHistoryToString(dto);
                 this.setState({
-                    battleText: text,
-                    currentBattleVisible: text !== ""
+                    battleText: reacapStringList,
+                    currentBattleVisible: reacapStringList.length !== 0
                 })
                 }, error => {
                 console.info(JSON.stringify(error))
@@ -66,60 +72,95 @@ export default class HomepagePanel extends React.Component<RouteComponentProps, 
 
     public render() {
         return (
-
             <Container>
-                <Row className="justify-content-md-center">
-                    <Col className="col-xs-12 col-sm-6 col-md-4 col-lg-3" style={{border: "1px solid #d8d8d8"}}>
-                        <Label>Shipy 1</Label>
-                        <ShipForm ref={this.shipForm1}/>
-                    </Col>
-                    <Col className="col-xs-12 col-sm-6 col-md-4 col-lg-3 offset-lg-3"
-                         style={{border: "1px solid #d8d8d8"}}>
-                        <Label>Shipy 2</Label>
-                        <ShipForm ref={this.shipForm2}/>
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-center">
-                    <Button onClick={this.postNewBattle} style={{marginBottom: "1rem"}}>New battle</Button>
-                </Row>
-                <Row>
-                    <InputGroup size="sm">
-                        <Label>Stage issue delay (sec) {"\u00a0"}</Label>
-                        <Input type="number" innerRef={this.stageIssueTimeRef} invalid={!this.isIssueTimeValid()}
-                               onChange={e => this.setState({stageIssueTime: e.target.value})}/>
-                        <FormFeedback>From 3 to 60 seconds</FormFeedback>
-                    </InputGroup>
-                </Row>
-                <Row>
-                    <Fade in={this.state.currentBattleVisible}>
-                        <Card>
-                            <CardBody>
-                                <CardTitle>Current battle</CardTitle>
-                                <CardHeader><Button onClick={this.cancelBattleRequest}
-                                                    style={{marginBottom: "1rem"}}>cancel battle</Button></CardHeader>
-                                <CardText style={{whiteSpace: "pre-wrap"}}>{this.state.battleText}</CardText>
-                            </CardBody>
-                        </Card>
-                    </Fade>
-                </Row>
+                <Col className="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
+                    <Row className="justify-content-center">
+                        <Col className="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+                            <Label>Army 1</Label>
+                            <ShipForm ref={this.shipForm1}/>
+                        </Col>
+                        <Col className="col-xs-12 col-sm-6 col-md-6 col-lg-5 offset-lg-2">
+                            <Label>Army 2</Label>
+                            <ShipForm ref={this.shipForm2}/>
+                        </Col>
+                    </Row>
+                    <Row style={{marginTop: "2rem"}}>
+                        <Col className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                            <InputGroup size="sm">
+                                <Label>Time between resolving following battle stages (sec):{"\u00a0"}</Label>
+                                <Input type="number" style={{maxWidth: "3rem"}}
+                                       innerRef={this.stageIssueTimeRef} invalid={!this.isIssueTimeValid()}
+                                       onChange={e => this.setState({stageIssueTime: e.target.value})}/>
+                                <FormFeedback>From 3 to 60 seconds</FormFeedback>
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+                            <Button onClick={this.postNewBattle} size="lg"
+                                    style={{marginBottom: "1rem", marginTop: "1rem"}}>New
+                                battle</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <hr style={{borderTop: "1px solid #3c2c18", minWidth: "100%"}}/>
+                    </Row>
+                    <Row>
+                        {this.state.currentBattleVisible &&
+                        (<Fade in={this.state.currentBattleVisible} style={{minWidth: "100%"}}>
+                                <Card>
+                                    <CardBody>
+                                        <CardHeader>Current battle
+                                            <Button size="sm" onClick={this.cancelBattleRequest}
+                                                    color="danger"
+                                                    id="cancelButtonId"
+                                                    style={{marginBottom: "1rem"}}>Cancel<br/>battle
+                                            </Button>
+                                            <Tooltip placement="top" isOpen={this.state.tooltipOpen}
+                                                     target="cancelButtonId"
+                                                     toggle={() => this.setState({tooltipOpen: !this.state.tooltipOpen})}
+                                            >Cancelling is penalized: Army 1 doesn't fire while retreating
+                                            </Tooltip>
+                                        </CardHeader>
+                                        <CardText
+                                            style={{whiteSpace: "pre-wrap"}}>{this.listFromBattleHistoryString(this.state.battleText)}</CardText>
+                                    </CardBody>
+                                </Card>
+                            </Fade>
+                        )}
+                    </Row>
+                </Col>
             </Container>);
     }
 
-    private battleHistoryToString(dto: BattleHistoryDTO) {
-        let text: string = "";
-        Object.keys(dto.battleRecapMap).forEach(dtoKey => {
-            text += `STAGE ${dtoKey} RECAP\n`;
+    private battleHistoryToString(dto: BattleHistoryDTO): BattleHistoryRecapString[] {
+        return Object.keys(dto.battleRecapMap).map(dtoKey => {
+            let text = "";
             const armyRecap = dto.battleRecapMap[dtoKey];
             Object.keys(armyRecap).filter(v => v === "army1Recap" || v === "army2Recap").forEach(armyRecapKey => {
                 text += `\tArmy: ${armyRecapKey}\n`;
                 const armyShips = armyRecap[armyRecapKey];
-                Object.keys(armyShips).forEach(shipType => {
-                    const count = armyShips[shipType];
-                    text += `\t\t${shipType}: ${count}\n`;
+                Object.keys(armyShips).forEach(destroyedOrLivingShipsMap => {
+                    const shipsStateName = armyShips[destroyedOrLivingShipsMap];
+                    if (Object.keys(shipsStateName).length !== 0) {
+                        text += `\t\t${destroyedOrLivingShipsMap}:\n`;
+                    }
+                    Object.keys(shipsStateName).forEach(shipType => {
+                        const count = shipsStateName[shipType];
+                        text += `\t\t\t${shipType}: ${count}\n`;
+                    })
                 })
-            })
+            });
+            return {stage: dtoKey, stageSummary: text};
         });
-        return text;
+    }
+
+    private listFromBattleHistoryString = (stages: BattleHistoryRecapString[]): any => {
+        let key = 0;
+        return (
+            <ListGroup>
+                {stages.map(stage => <ListStageGroupItem key={key += 1} text={stage}/>)}
+            </ListGroup>)
     }
 
     private postNewBattle = () => {
