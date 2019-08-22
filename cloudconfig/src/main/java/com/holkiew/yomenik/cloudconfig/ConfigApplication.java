899db51be51a75b3426ab0f92fc879c7b25d189d
Hubert <olkiewiczhubert@gmail.com>
2019-08-22 13:55:54 +0200

    jars

--- cloudconfig/src/main/java/com/holkiew/yomenik/cloudconfig/ConfigApplication.java
+++ cloudconfig/src/main/java/com/holkiew/yomenik/cloudconfig/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.holkiew.yomenik.gateway;
+package com.holkiew.yomenik.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
